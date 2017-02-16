91aad6013cd74bb4dd2682f6c843c9833009e3e2
antoinekhouri <antoine.khouri@mail.mcgill.ca>
2017-02-16 16:46:00 -0500

    Added TaOffer and GraderOffer

---
+++ TeachingAssistantManagementSystem/src/ca/mcgill/ecse321/teachingassistantmanagementsystem/ump/GraderOffer.java
@@ -0,0 +1,57 @@
+/*PLEASE DO NOT EDIT THIS CODE*/
+/*This code was generated using the UMPLE 1.24.0-dab6b48 modeling language!*/
+
+package ca.mcgill.ecse321.teachingassistantmanagementsystem.ump;
+
+// line 46 "../../../../../TeachingAssistantManagementSystem.ump"
+public class GraderOffer extends JobOffer
+{
+
+  //------------------------
+  // MEMBER VARIABLES
+  //------------------------
+
+  //GraderOffer Attributes
+  private int capacity;
+
+  //------------------------
+  // CONSTRUCTOR
+  //------------------------
+
+  public GraderOffer(int aWorkHours, Course aCourse, Application aApplication, int aCapacity)
+  {
+    super(aWorkHours, aCourse, aApplication);
+    capacity = aCapacity;
+  }
+
+  //------------------------
+  // INTERFACE
+  //------------------------
+
+  public boolean setCapacity(int aCapacity)
+  {
+    boolean wasSet = false;
+    capacity = aCapacity;
+    wasSet = true;
+    return wasSet;
+  }
+
+  public int getCapacity()
+  {
+    return capacity;
+  }
+
+  public void delete()
+  {
+    super.delete();
+  }
+
+
+  public String toString()
+  {
+    String outputString = "";
+    return super.toString() + "["+
+            "capacity" + ":" + getCapacity()+ "]"
+     + outputString;
+  }
+}
---
+++ TeachingAssistantManagementSystem/src/ca/mcgill/ecse321/teachingassistantmanagementsystem/ump/TaOffer.java
@@ -0,0 +1,57 @@
+/*PLEASE DO NOT EDIT THIS CODE*/
+/*This code was generated using the UMPLE 1.24.0-dab6b48 modeling language!*/
+
+package ca.mcgill.ecse321.teachingassistantmanagementsystem.ump;
+
+// line 41 "../../../../../TeachingAssistantManagementSystem.ump"
+public class TaOffer extends JobOffer
+{
+
+  //------------------------
+  // MEMBER VARIABLES
+  //------------------------
+
+  //TaOffer Attributes
+  private int capacity;
+
+  //------------------------
+  // CONSTRUCTOR
+  //------------------------
+
+  public TaOffer(int aWorkHours, Course aCourse, Application aApplication, int aCapacity)
+  {
+    super(aWorkHours, aCourse, aApplication);
+    capacity = aCapacity;
+  }
+
+  //------------------------
+  // INTERFACE
+  //------------------------
+
+  public boolean setCapacity(int aCapacity)
+  {
+    boolean wasSet = false;
+    capacity = aCapacity;
+    wasSet = true;
+    return wasSet;
+  }
+
+  public int getCapacity()
+  {
+    return capacity;
+  }
+
+  public void delete()
+  {
+    super.delete();
+  }
+
+
+  public String toString()
+  {
+    String outputString = "";
+    return super.toString() + "["+
+            "capacity" + ":" + getCapacity()+ "]"
+     + outputString;
+  }
+}
