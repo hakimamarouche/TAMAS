8792236288d24f8e12cbee887ea6611bd2782f3a
antoinekhouri <antoine.khouri@mail.mcgill.ca>
2017-02-20 12:49:22 -0500

    Added applyForJob

--- TeachingAssistantManagementSystem/src/ca/mcgill/ecse321/teachingassistantmanagementsystem/controller/TeachingAssistantManagementSystemController.java
+++ TeachingAssistantManagementSystem/src/ca/mcgill/ecse321/teachingassistantmanagementsystem/controller/TeachingAssistantManagementSystemController.java
@@ -2,10 +2,13 @@ package ca.mcgill.ecse321.teachingassistantmanagementsystem.controller;
 
 import java.util.List;
 
+import ca.mcgill.ecse321.teachingassistantmanagementsystem.ump.Applicant;
+import ca.mcgill.ecse321.teachingassistantmanagementsystem.ump.Application;
 import ca.mcgill.ecse321.teachingassistantmanagementsystem.ump.Course;
 import ca.mcgill.ecse321.teachingassistantmanagementsystem.ump.GraderOffer;
 import ca.mcgill.ecse321.teachingassistantmanagementsystem.ump.Instructor;
 import ca.mcgill.ecse321.teachingassistantmanagementsystem.ump.JobManager;
+import ca.mcgill.ecse321.teachingassistantmanagementsystem.ump.JobOffer;
 import ca.mcgill.ecse321.teachingassistantmanagementsystem.ump.TaOffer;
 
 public class TeachingAssistantManagementSystemController {
@@ -44,5 +47,21 @@ public class TeachingAssistantManagementSystemController {
 		newCourse.addJob(newTaJob);
 		newCourse.addJob(newGraderJob);
 		jm.addCourse(newCourse);
+	}
+	public void applyForJob(Applicant newApplicant, String experience, JobOffer... allJobs) throws InvalidInputException{
+		String error = "";
+		if(newApplicant == null){
+			error = error + "No applicant selected";
+		}
+		if(experience == null){
+			error = error + "Experience can not be empty";
+		}
+		if(allJobs.length<1 || allJobs.length>3){
+			error = error + "Applicant can only apply for one to three jobs.";
+		}
+		Application newApplication = new Application(experience, newApplicant, allJobs);
+		for(JobOffer nextJob : allJobs) {
+			nextJob.addApplication(newApplication);
+		}
 	}
 }
