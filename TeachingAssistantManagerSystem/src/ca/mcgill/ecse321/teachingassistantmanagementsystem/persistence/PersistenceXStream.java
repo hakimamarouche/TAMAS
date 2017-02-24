0504b7573d76bad85ee8602a99a37f8b63806e1e
hakimamarouche <hakim.amarouche@mail.mcgill.ca>
2017-02-23 22:06:40 -0500

    fixed PersistenceXStream (Junit test working)

--- TeachingAssistantManagerSystem/src/ca/mcgill/ecse321/teachingassistantmanagementsystem/persistence/PersistenceXStream.java
+++ TeachingAssistantManagerSystem/src/ca/mcgill/ecse321/teachingassistantmanagementsystem/persistence/PersistenceXStream.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 import com.thoughtworks.xstream.XStream;
 
 import ca.mcgill.ecse321.teachingassistantmanagementsystem.ump.Course;
+import ca.mcgill.ecse321.teachingassistantmanagementsystem.ump.Department;
 import ca.mcgill.ecse321.teachingassistantmanagementsystem.ump.JobManager;
 
 
@@ -16,13 +17,13 @@ public abstract class PersistenceXStream {
 	private static XStream xstream = new XStream();
     private static String filename = "data.xml";
     
-    public static JobManager initializeModelManager(String fileName){
-    	JobManager jm;
+    public static Department initializeModelManager(String fileName){
+    	Department dpt;
     	setFilename(fileName);
     	setAlias("course", Course.class);
     	File file = new File(fileName);
         if (file.exists()) {
-            jm = (JobManager) loadFromXMLwithXStream();
+            dpt = (Department) loadFromXMLwithXStream();
         } else {
         	try {
                 file.createNewFile();
@@ -30,10 +31,10 @@ public abstract class PersistenceXStream {
                 e.printStackTrace();
                 System.exit(1);
             }
-            jm = new JobManager(4);
-            saveToXMLwithXStream(jm);
+            dpt = new Department(4);
+            saveToXMLwithXStream(dpt);
         }
-        return jm;
+        return dpt;
         }
     
     public static boolean saveToXMLwithXStream(Object obj) {
