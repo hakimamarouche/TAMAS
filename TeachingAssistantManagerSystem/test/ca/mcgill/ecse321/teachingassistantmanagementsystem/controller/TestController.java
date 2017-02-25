f5fa3a1b5309ed527d8a544f19abcf26fbfb8edc
hakimamarouche <hakim.amarouche@mail.mcgill.ca>
2017-02-25 16:29:25 -0500

    tried to implement first test on createJobPosting but getting an error.

--- TeachingAssistantManagerSystem/test/ca/mcgill/ecse321/teachingassistantmanagementsystem/controller/TestController.java
+++ TeachingAssistantManagerSystem/test/ca/mcgill/ecse321/teachingassistantmanagementsystem/controller/TestController.java
@@ -12,10 +12,13 @@ import org.junit.Test;
 
 import ca.mcgill.ecse321.teachingassistantmanagementsystem.persistence.PersistenceXStream;
 import ca.mcgill.ecse321.teachingassistantmanagementsystem.ump.Department;
+import ca.mcgill.ecse321.teachingassistantmanagementsystem.ump.Instructor;
+import ca.mcgill.ecse321.teachingassistantmanagementsystem.ump.JobManager;
 
 public class TestController {
 
 	private Department dpt;
+	private JobManager jm;
 	
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
@@ -28,17 +31,46 @@ public class TestController {
 
 	@Before
 	public void setUp() throws Exception {
+		// should it be the opposite? --> using constructor Department(JobManager)
 		dpt = new Department(4);
+		jm = new JobManager(4, dpt);
+		//however, it seems redundant... something is wrong with the class diagram I think
 	}
 
 	@After
 	public void tearDown() throws Exception {
-		dpt.delete();
+		jm.delete();
 	}
 
 	@Test
-	public void testCreateCourses() {
-		fail("Not yet implemented");
+	public void testCreateCourses() throws InvalidInputException {
+		assertEquals(0, jm.getCourses().size());
+
+		int taHours = 40;
+		int graderHours = 40;
+		int courseCredit = 4;
+		String courseID = "ECSE 321";
+		int budget = 10000000;
+		int studentsEnrolled = 50;
+		Instructor courseInstructor = new Instructor();
+		int taOfferCapacity = 50;
+		int graderOfferCapacity = 50;
+
+		TeachingAssistantManagementSystemController tamsc = new TeachingAssistantManagementSystemController(jm);
+		tamsc.createJobPosting(taHours, graderHours, courseCredit, courseID, budget, studentsEnrolled, courseInstructor, taOfferCapacity, graderOfferCapacity);
+
+		  // check model in memory
+		  checkResultJobPosting(courseID);
+
+		  jm = (JobManager) PersistenceXStream.loadFromXMLwithXStream();
+		  
+		// check file contents
+		  checkResultJobPosting(courseID);
+	}
+
+	private void checkResultJobPosting(String courseID) {
+		assertEquals(1, jm.getCourses().size());
+		  assertEquals(courseID, jm.getCourse(0).getCourseId());
 	}
 
 }
